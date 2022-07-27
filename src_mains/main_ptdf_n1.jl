1c0fb3750fd6279531cbd1b1bf51cf110210e71b
Aziz Jegham <aziz.jegham@eurodecision.com>
2022-07-27 12:08:37 +0200

    main_ptdf_n1 : generate ptdf N and N-1 for some matpower networks

--- src_mains/main_ptdf_n1.jl
+++ src_mains/main_ptdf_n1.jl
@@ -16,6 +16,46 @@ cd(root_path)
 include(joinpath(root_path, "src", "PTDF.jl"));
 
 
+MATPOWER_NETWORKS = [
+    "case4gs",
+    "case5",
+    "case6ww",
+    "case9",
+    "case9Q",
+    "case9target",
+    "case14",
+    "case24_ieee_rts",
+    "case30",
+    "case30pwl",
+    "case30Q",
+    "case39",
+    "case57",
+    "case89pegase",
+    "case118",
+    "case145",
+    "case300",
+    # "case1354pegase",
+    # "case13659pegase",
+    # "case1888rte",
+    # "case1951rte",
+    # "case2383wp",
+    # "case2736sp",
+    # "case2737sop",
+    # "case2746wop",
+    # "case2746wp",
+    # "case2848rte",
+    # "case2868rte",
+    # "case2869pegase",
+    # "case3012wp",
+    # "case3120sp",
+    # "case3375wp",
+    # "case6468rte",
+    # "case6470rte",
+    # "case6495rte",
+    # "case6515rte",
+    # "case9241pegase",
+];
+
 #########################
 # INPUT & PARAMS
 #########################
@@ -35,8 +75,10 @@ function main(input_path, ref_bus_num, distributed, eps_diag)
     PTDF.compute_and_write_n_non_bridges(network, ref_bus_num, distributed, eps_diag, input_path, out_path)
 end
 
-main(input_path, ref_bus_num, distributed, eps_diag)
-
-
-# input_path = joinpath(@__DIR__, "..", "data_matpower", "case118")
 # main(input_path, ref_bus_num, distributed, eps_diag)
+
+for matpower_case in MATPOWER_NETWORKS
+    @info matpower_case
+    matpower_path = joinpath(@__DIR__, "..", "data_matpower", matpower_case)
+    main(matpower_path, ref_bus_num, distributed, eps_diag)
+end
